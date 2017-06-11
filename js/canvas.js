0e566bbc571cd0a7ecf2a85444ccad2f54198582
Massimiliano Salerno <imafa82@gmail.com>
2017-06-11 17:00:57 +0200

    Inizio creazione paesaggio con canvas

--- js/canvas.js
+++ js/canvas.js
@@ -1,85 +1,146 @@
 var canvas = document.querySelector("#lienzo");
-var ctx = canvas.getContext("2d");
+var ctx = canvas.getContext('2d');
+var alfa = 0.50;
 
-/*=====================================================
-QUADRO
-======================================================*/
-// RIEMPIMENTO
-ctx.fillStyle = "cyan";
-// ctx.fillRect(x1, y1, x2, y2);
-ctx.fillRect(0, 0, 100, 100);
+//Cielo
 
-//CONTORNO
-ctx.lineWidth = 5;
-ctx.strokeStyle ="rgba(255, 0, 255, 1)";
-ctx.rect(0,0,100,100);
-ctx.stroke();
-
-/*=====================================================
-CERCHIO
-======================================================*/
-
-ctx.beginPath();
-//ctx.arc(x, y, radius, startAngle, endAngle);
-ctx.arc(300, 300, 60, 0, 2* Math.PI);
-ctx.fillStyle = "green";
-ctx.fill();
-
-//CONTORNO CERCHIO
-ctx.lineWidth = 5;
-ctx.strokeStyle ="purple";
-ctx.stroke();
-
-/*=====================================================
-LINEAS
-======================================================*/
-
-ctx.beginPath();
-ctx.moveTo(0, 0);
-ctx.lineTo(200, 200);
-ctx.lineTo(400, 200);
-ctx.lineTo(600, 400);
-ctx.lineTo(800, 200);
-ctx.lineTo(1000, 200);
-ctx.lineTo(1000, 0);
-// Contorno linea
-ctx.lineWidth = 5;
-ctx.stroke();
-//Riempimento linea
-ctx.fillStyle = "rgba(0,0,255,.6)";
-ctx.fill();
-
-/*=====================================================
-CURVA
-======================================================*/
-
-ctx.beginPath();
-ctx.moveTo(0,500);
-
-ctx.bezierCurveTo(200,300, 400, 400, 500, 500);
-
-// Contorno curva
-ctx.lineWidth = 5;
-ctx.strokeStyle = 'rgba(100,0,0,1)';
-ctx.stroke();
-
-//Riempimento curva
-ctx.fillStyle = 'rgba(255,0,0,1)';
-ctx.fill();
-
-/*=====================================================
-GRADIENTE
-======================================================*/
-var grd = ctx.createLinearGradient(0,300,100,300);
-grd.addColorStop(0, "red");
-grd.addColorStop(1, "yellow");
+var grd = ctx.createLinearGradient(0,0,0,600);
+grd.addColorStop(0,"rgba(0,0,255,"+alfa+")");
+grd.addColorStop(1, "white");
 ctx.fillStyle = grd;
-ctx.fillRect(0,300, 100, 100);
+ctx.fillRect(0,0,1000,500);
 
-var grd2 = ctx.createRadialGradient(890,350, 5, 900, 350, 120);
-grd2.addColorStop(0, "white");
-grd2.addColorStop(1, "black");
-ctx.beginPath();
-ctx.arc(900, 400, 100, 0, 7);
+
+//Oceano
+
+var grd2 = ctx.createLinearGradient(0,400,0,500);
+grd2.addColorStop(0,"rgba(0,0,255,"+alfa+")");
+grd2.addColorStop(1, "white");
 ctx.fillStyle = grd2;
+ctx.fillRect(0,400,1000,100);
+
+
+//Montagna 1
+ctx.beginPath();
+ctx.fillStyle = "rgba(100,0,255,"+alfa+")";
+ctx.moveTo(0,400);
+ctx.lineTo(200,100);
+ctx.lineTo(400,400);
+ctx.fill();
+
+//Montagna 2
+ctx.beginPath();
+ctx.fillStyle = "rgba(100,0,255,"+alfa+")";
+ctx.moveTo(200,400);
+ctx.lineTo(400,100);
+ctx.lineTo(600,400);
+ctx.fill();
+
+//Montagna 3
+ctx.beginPath();
+ctx.fillStyle = "rgba(100,0,255,"+alfa+")";
+ctx.moveTo(400,400);
+ctx.lineTo(600,100);
+ctx.lineTo(800,400);
+ctx.fill();
+
+//Montagna 4
+ctx.beginPath();
+ctx.fillStyle = "rgba(100,0,255,"+alfa+")";
+ctx.moveTo(600,400);
+ctx.lineTo(800,100);
+ctx.lineTo(1000,400);
+ctx.fill();
+
+// Neve Montagna 1
+ctx.beginPath();
+ctx.fillStyle = "rgba(255, 255, 255, "+alfa+")";
+ctx.moveTo(133, 200);
+ctx.lineTo(200, 100);
+ctx.lineTo(200, 300);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = "rgba(210, 210, 255, "+alfa+")";
+ctx.moveTo(200, 300);
+ctx.lineTo(200, 100);
+ctx.lineTo(267, 200);
+ctx.fill();
+
+// Neve Montagna 2
+ctx.beginPath();
+ctx.fillStyle = "rgba(255, 255, 255, "+alfa+")";
+ctx.moveTo(333, 200);
+ctx.lineTo(400, 100);
+ctx.lineTo(400, 300);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = "rgba(210, 210, 255, "+alfa+")";
+ctx.moveTo(400, 300);
+ctx.lineTo(400, 100);
+ctx.lineTo(467, 200);
+ctx.fill();
+
+// Neve Montagna 3
+ctx.beginPath();
+ctx.fillStyle = "rgba(255, 255, 255, "+alfa+")";
+ctx.moveTo(533, 200);
+ctx.lineTo(600, 100);
+ctx.lineTo(600, 300);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = "rgba(210, 210, 255, "+alfa+")";
+ctx.moveTo(600, 300);
+ctx.lineTo(600, 100);
+ctx.lineTo(667, 200);
+ctx.fill();
+
+// Neve Montagna 4
+ctx.beginPath();
+ctx.fillStyle = "rgba(255, 255, 255, "+alfa+")";
+ctx.moveTo(733, 200);
+ctx.lineTo(800, 100);
+ctx.lineTo(800, 300);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = "rgba(210, 210, 255, "+alfa+")";
+ctx.moveTo(800, 300);
+ctx.lineTo(800, 100);
+ctx.lineTo(867, 200);
+ctx.fill();
+
+
+//Monte 1
+
+ctx.beginPath();
+ctx.fillStyle = "rgba(10, 0, 150, "+alfa+")";
+ctx.moveTo(0,400);
+ctx.lineTo(200,300);
+ctx.lineTo(200,450);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = "rgba(10, 0, 100, "+alfa+")";
+ctx.moveTo(200,450);
+ctx.lineTo(200,300);
+ctx.lineTo(400,400);
+ctx.fill();
+
+//Monte 2
+
+ctx.beginPath();
+ctx.fillStyle = "rgba(10, 0, 150, "+alfa+")";
+ctx.moveTo(600,400);
+ctx.lineTo(800,300);
+ctx.lineTo(800,450);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = "rgba(10, 0, 100, "+alfa+")";
+ctx.moveTo(800,450);
+ctx.lineTo(800,300);
+ctx.lineTo(1000,400);
 ctx.fill();
--- js/script.js
+++ js/script.js
@@ -1,46 +1,43 @@
-var contenitore = document.querySelector("#contenitore");
+var contenedor = document.querySelector("#contenedor");
 var ampliarLienzo = document.querySelector("#lienzo");
-var btnAmpliar = document.querySelector('#btnAmpliar');
-var ampliarControl = true;
-btnAmpliar.onclick = controlAmpliar;
+var btnAmpliar = document.querySelector("#btnAmpliar");
 
-function controlAmpliar(){
-    if(ampliarControl){
-        ampliar();
-    } else {
-        reducir();
-    }
-}
 function ampliar(){
-    contenitore.style.width = "100%";
-    contenitore.style.height = "100vh";
-    contenitore.style.margin = "0";
 
-    ampliarLienzo.style.width = "100%";
-    ampliarLienzo.style.height = "100vh";
-    ampliarLienzo.style.backgroundSize = "cover";
-    ampliarLienzo.style.backgroundRepeat = "no-repeat";
+	contenedor.style.width="100%";
+	contenedor.style.height="100vh";
+	contenedor.style.margin="0";
 
-    btnAmpliar.innerHTML = "Ridurre Canvas";
-    btnAmpliar.style.position = "fixed";
-    btnAmpliar.style.top = "10px";
-    btnAmpliar.style.left = "10px";
-    btnAmpliar.style.zIndex = "1";
-    ampliarControl = false;
+	ampliarLienzo.style.width = "100%";
+	ampliarLienzo.style.height = "100vh";
+	ampliarLienzo.style.backgroundSize = "cover";
+	ampliarLienzo.style.backgroundRepeat = "no-repeat";
+
+	btnAmpliar.innerHTML = "Reducir Canvas";
+	btnAmpliar.style.position = "fixed";
+	btnAmpliar.style.top = "10px";
+	btnAmpliar.style.left = "10px";
+	btnAmpliar.style.zIndex = "1";
+
+	btnAmpliar.setAttribute("onClick","reducir()");
+
 }
 
 function reducir(){
-    contenitore.style.width = "1000px";
-    contenitore.style.height = "500px";
-    contenitore.style.margin = "5vh auto";
 
-    ampliarLienzo.style.width = "1000px";
-    ampliarLienzo.style.height = "500px";
-    
-    btnAmpliar.innerHTML = "Ingrandire Canvas";
-    btnAmpliar.style.position = "relative";
-    btnAmpliar.style.top = "0";
-    btnAmpliar.style.left = "0";
-    btnAmpliar.style.zIndex = "0";
-    ampliarControl = true;
+	contenedor.style.width = "1000px";
+	contenedor.style.height = "500px";
+	contenedor.style.margin = "5vh auto";
+
+	ampliarLienzo.style.width = "1000px";
+	ampliarLienzo.style.height = "500px";
+
+	btnAmpliar.innerHTML = "Ampliar Canvas";
+	btnAmpliar.style.position = "relative";
+	btnAmpliar.style.top = "0";
+	btnAmpliar.style.left = "0";
+	btnAmpliar.style.zIndex = "0";
+
+	btnAmpliar.setAttribute("onClick","ampliar()");
+
 }
